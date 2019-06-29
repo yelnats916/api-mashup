0b045e1a42a5f4276e1d7b8bd68f9f299884b76f
Stanley Tang <yelnats916@gmail.com>
2019-06-29 00:07:28 -0700

    first commit

---
+++ src/main/java/com/apiMashup/App.java
@@ -0,0 +1,13 @@
+package com.apiMashup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
