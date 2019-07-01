5dcd9055ba8292be9dd5fbfad003e40ebf79f089
Stanley Tang <yelnats916@gmail.com>
2019-06-30 18:56:53 -0700

    java jar works

--- src/main/java/com/apiMashup/Application.java
+++ src/main/java/com/apiMashup/Application.java
@@ -12,10 +12,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class All {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(All.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
     /*
